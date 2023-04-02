5071a007451a5eee38ee8b3a6c4c2b397138e4e3
an1maEnjoyer <10y.oanimeenjoyer@gmail.com>
2023-04-02 16:56:00 +0300

    add changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -13,16 +13,19 @@ import React, { useEffect } from "react";
 import axios from '../../axios.js';
 import { groups } from '../../groups';
 
+import DropDownInput from '../DropDownInput'
+
 function App() {
+
     let today = new Date();
-    const options = { weekday: "short", month: "short", day: "numeric" };
-    let now = today.toLocaleString("ru-RU", options);
-    const [inputGroup, setInputGroup] = useState('');
+    const options_for = { weekday: "short", month: "short", day: "numeric" };
+    let now = today.toLocaleString("ru-RU", options_for);
+    const [searchValue, setSearchValue] = useState("");
     const [currentShedule, setCurrentShedule] = useState({});
     const myRef = useRef(null);
-
+    console.log(searchValue)
     useEffect(() => {
-        const chosenGroup = Object.entries(groups).find((item) => item[0] == inputGroup)
+        const chosenGroup = Object.entries(groups).find((item) => item[0].toLowerCase() == searchValue.toLowerCase())
         console.log(`выбрал группу ${chosenGroup}`)
         if (chosenGroup) {
             axios.get(`/groups?faculty=${chosenGroup[1].faculty}&year=${chosenGroup[1].year}&group=${chosenGroup[1].group}`).then(res => {
@@ -30,28 +33,38 @@ function App() {
                 console.log(`получил данные от ${chosenGroup[0]}`)
             })
         }
-    }, [inputGroup]);
-
+    }, [searchValue]);
     const flipPage = () => myRef.current.scrollIntoView({ behavior: "smooth" });
-
     const optionsWeekDay = { weekday: "short" };
     let weekDay = today.toLocaleString("ru-RU", optionsWeekDay);
 
     const [activeDay, setActiveDay] = useState(
         weekDay.charAt(0).toUpperCase() + weekDay.slice(1)
     );
+    const sorted = Object.entries(groups).filter((item) => {
 
+        return item[0].toLowerCase().includes(searchValue.toLowerCase())
+    })
+    const options = sorted.map((item) => {
+        return {
+            label: item[0],
+            value: item[0].toLowerCase()
+        }
+    })
     return (
         <>
-            <Video flipPage={flipPage} inputGroup={inputGroup} setInputGroup={setInputGroup} groups={groups}></Video>
+            <Video flipPage={flipPage} searchValue={searchValue} setSearchValue={setSearchValue} groups={groups}></Video>
             <div className="container">
                 <Header myRef={myRef}></Header>
+
                 <main className={styles.main}>
                     <article className={styles.settings}>
+
                         <Calendar
                             setActiveDay={setActiveDay}
                             activeDay={activeDay}
                         ></Calendar>
+
                         <div className={styles.groupInformation}>
                             <div className={styles.groupElement}>
                                 <span className={styles.groupText}>Первый курс</span>
@@ -71,9 +84,10 @@ function App() {
                         </div>
                     </article>
                     <article className={styles.content}>
-                        <Shedule activeDay={activeDay}></Shedule>
+                        <Shedule activeDay={activeDay} shedule={currentShedule}></Shedule>
                     </article>
                 </main>
+                <DropDownInput placeHolder="Select..." options={options.slice(0, 10)}></DropDownInput>
                 <Footer></Footer>
             </div>
         </>
